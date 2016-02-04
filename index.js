1e0d29bb73516fc17a1e08c46756b6414e39d0b3
Matt Bell <mappum@gmail.com>
2016-02-03 16:33:45 -0800

    Build with babel on publish

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var stream = require('./src/stream.js')
+var stream = require('./lib/stream.js')
 
 module.exports = {
   createDecodeStream: stream.createDecodeStream,
   createEncodeStream: stream.createEncodeStream,
-  dataTypes: require('./src/dataTypes.js'),
-  messages: require('./src/messages.js')
+  dataTypes: require('./lib/dataTypes.js'),
+  messages: require('./lib/messages.js')
 }
