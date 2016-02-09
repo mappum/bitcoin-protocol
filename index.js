807862ed5ca86aede2467b055eb35cad203df5b0
Matt Bell <mappum@gmail.com>
2016-02-08 19:41:56 -0800

    Added encode/decode aliases for createXStream methods

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var stream = require('./lib/stream.js')
 
 module.exports = {
   createDecodeStream: stream.createDecodeStream,
+  decode: stream.createDecodeStream,
   createEncodeStream: stream.createEncodeStream,
+  encode: stream.createEncodeStream,
   dataTypes: require('./lib/dataTypes.js'),
   messages: require('./lib/messages.js')
 }
